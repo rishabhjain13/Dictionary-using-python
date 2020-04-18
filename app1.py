72ddc69daffd59a2bbec0a8042d496e13bbe12cb
rishabhjain13 <51086907+rishabhjain13@users.noreply.github.com>
2020-04-18 21:55:16 +0530

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,33 @@
+import json
+from difflib import get_close_matches
+
+data = json.load(open("C:/Users/rishabh jain/Documents/python/a1/data.json"))
+
+def translate(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif w.title() in data:
+        return data[w.title()]
+    elif w.upper() in data:
+        return data[w.upper()]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
+        yn = yn.upper()
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == "N":
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "We didn't understand your entry."
+    else:
+        return "The word doesn't exist. Please double check it."
+
+while True:
+    word = input("Enter word: ")
+    output = translate(word)
+    if type(output) == list:
+        for item in output:
+            print(item)
+    else:
+        print(output)
